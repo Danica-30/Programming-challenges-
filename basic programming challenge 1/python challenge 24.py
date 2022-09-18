7d6b4fb04a25735d19333fe2e17d754185774a7a
Danica-30 <113169691+Danica-30@users.noreply.github.com>
2022-09-18 13:26:00 +0100

    Add files via upload

---
+++ basic programming challenge 1/python challenge 24.py
@@ -0,0 +1,24 @@
+def two_numbers(nx,ns):
+    x= "x"*(nx)
+    y=" "*(ns)
+    total=y+x
+    return total
+
+
+
+def repeat():
+    flag= True
+    spaces=int(input("enter 1st number: "))
+    x=int(input("enter 2nd number: "))
+    while flag == True:
+     print(f"{two_numbers(x,spaces)}")
+repeat()
+            
+       
+
+
+
+      
+
+
+ 
