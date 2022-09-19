3e317c70a522028be4c703dd4ff11d5d62991fd5
Danica-30 <113169691+Danica-30@users.noreply.github.com>
2022-09-19 20:38:31 +0100

    Add files via upload

---
+++ basic programming challenge 1/dictonary task 1 .py
@@ -0,0 +1,21 @@
+
+#def splitdate():
+    #date= input("enter a date in this format dd-mmm-yy: ")
+    #newdate=date.replace("-",","))
+    #tupledate=
+#splitdate()
+
+def splitdate(day,month,year):
+    tupledate= tuple((day,month,year))
+    print(tupledate)
+
+day1= int(input(f"please enter the day of the date: "))
+month1= input(f"please enter the month of the date(eg: january,march... ")
+year1= input(f"please enter the year of the date: ")
+
+monthdict={"january":1,"february":2,"march":3,"april":4,"may":5,"june":6,"july":7,"august":8,"september":9,"october":10,"november":11,"december":12}
+if month
+
+splitdate(day1,month1,year1)
+    
+
